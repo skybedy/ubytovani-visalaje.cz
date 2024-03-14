7fc8c25adf59e53b584a31e0af13501e05ed066a
skybedy <skybedy@gmail.com>
2024-03-14 14:26:00 +0000

    Fix PHP_SELH na SCRIPT_NAME

--- menu.php
+++ menu.php
@@ -1,9 +1,9 @@
 <a href="index.php" class="menu_hl" title="Novinky">Novinky</a> &nbsp; &nbsp;
-<a href="ubytovani.php" class="menu_hl<?php if (strstr($_SERVER[PHP_SELF],'ubytovani')) echo"_on"?>" title="Nabídka ubytování">Nabídka ubytování</a> &nbsp; &nbsp;
-<a href="sluzby.php" class="menu_hl<?php if (strstr($_SERVER[PHP_SELF],'sluzby')) echo"_on"?>" title="Služby">Služby</a> &nbsp; &nbsp;
-<a href="fotogalerie.php" class="menu_hl<?php if (strstr($_SERVER[PHP_SELF],'fotogalerie')) echo"_on" ?>" title="Fotogalerie">Fotogalerie</a> &nbsp; &nbsp;
-<a href="zajimavosti.php" class="menu_hl<?php if (strstr($_SERVER[PHP_SELF],'zajimavosti')) echo"_on"?>" title="Zajímavosti v okolí">Zajímavosti v okolí</a> &nbsp; &nbsp;
-<a href="aktivity.php" class="menu_hl<?php if (strstr($_SERVER[PHP_SELF],'aktivity')) echo"_on"?>" title="Aktivity v okolí">Aktivity v okolí</a> &nbsp; &nbsp;
-<a href="mapa.php" class="menu_hl<?php if (strstr($_SERVER[PHP_SELF],'mapa')) echo"_on"?>" title="Kde nás najdete">Kde nás najdete</a> &nbsp; &nbsp;
-<a href="napiste.php" class="menu_hl<?php if (strstr($_SERVER[PHP_SELF],'napiste')) echo"_on"?>" title="Napište nám">Napište nám</a> &nbsp; &nbsp;
-<a href="kontakty.php" class="menu_hl<?php if (strstr($_SERVER[PHP_SELF],'kontakty')) echo"_on"?>" title="Kontakty">Kontakty</a>
+<a href="ubytovani.php" class="menu_hl<?php if (strstr($_SERVER[SCRIPT_NAME],'ubytovani')) echo"_on"?>" title="Nabídka ubytování">Nabídka ubytování</a> &nbsp; &nbsp;
+<a href="sluzby.php" class="menu_hl<?php if (strstr($_SERVER[SCRIPT_NAME],'sluzby')) echo"_on"?>" title="Služby">Služby</a> &nbsp; &nbsp;
+<a href="fotogalerie.php" class="menu_hl<?php if (strstr($_SERVER[SCRIPT_NAME],'fotogalerie')) echo"_on" ?>" title="Fotogalerie">Fotogalerie</a> &nbsp; &nbsp;
+<a href="zajimavosti.php" class="menu_hl<?php if (strstr($_SERVER[SCRIPT_NAME],'zajimavosti')) echo"_on"?>" title="Zajímavosti v okolí">Zajímavosti v okolí</a> &nbsp; &nbsp;
+<a href="aktivity.php" class="menu_hl<?php if (strstr($_SERVER[SCRIPT_NAME],'aktivity')) echo"_on"?>" title="Aktivity v okolí">Aktivity v okolí</a> &nbsp; &nbsp;
+<a href="mapa.php" class="menu_hl<?php if (strstr($_SERVER[SCRIPT_NAME],'mapa')) echo"_on"?>" title="Kde nás najdete">Kde nás najdete</a> &nbsp; &nbsp;
+<a href="napiste.php" class="menu_hl<?php if (strstr($_SERVER[SCRIPT_NAME],'napiste')) echo"_on"?>" title="Napište nám">Napište nám</a> &nbsp; &nbsp;
+<a href="kontakty.php" class="menu_hl<?php if (strstr($_SERVER[SCRIPT_NAME],'kontakty')) echo"_on"?>" title="Kontakty">Kontakty</a>
